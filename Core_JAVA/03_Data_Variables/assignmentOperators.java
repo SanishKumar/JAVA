534b648e4f6579ba44dcbbd40238f65babb1207a
SanishKumar <18sanishkumar@gmail.com>
2024-01-20 21:24:18 +0530

    1:58:00

--- Core_JAVA/03_Data_Variables/assignmentOperators.java
+++ Core_JAVA/03_Data_Variables/assignmentOperators.java
@@ -1,9 +1,12 @@
 public class assignmentOperators {
     public static void main(String args[]){
         int num1 = 7;
-        int num2 = 5;
+        // int num2 = 5;
 
-        int result = num1 / num2;
-        System.out.println(result);
+        // int result = num1 / num2;
+
+        num1 = num1 + 2;
+
+        System.out.println(num1);
     }
 }
---
+++ Core_JAVA/04_Conditional_Statements/ifElse.java
@@ -0,0 +1,10 @@
+public class ifElse {
+    public static void main(String[] args) {
+        int x = 18;
+
+        if(x>10)
+            System.out.println("Hello");
+        else
+            System.out.println("Bye");
+    }
+}
