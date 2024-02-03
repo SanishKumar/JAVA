cb6341f8024ad9faffb39f185a041719cd9281f9
SanishKumar <18sanishkumar@gmail.com>
2024-02-03 21:33:15 +0530

    2D Arrays

--- Core_JAVA/07_Arrays/02_MultiDimensionalArray.java
+++ Core_JAVA/07_Arrays/02_MultiDimensionalArray.java
@@ -1,5 +1,6 @@
 class MultiDimensionalArray {
     public static void main(String[] args) {
+        // int nums[][] = new int[3][4];
         int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
