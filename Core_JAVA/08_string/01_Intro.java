a3a8c7605cbff9e8ffe5f89e188f869aa55e0256
SanishKumar <18sanishkumar@gmail.com>
2024-02-10 22:52:10 +0530

    String Buffer

--- Core_JAVA/08_string/01_Intro.java
+++ Core_JAVA/08_string/01_Intro.java
@@ -1,8 +1,23 @@
 class Intro {
     public static void main(String[] args) {
         // String name = "Sanish";
-        String name = new String("Sanish");
-        name = name + " Kumar";
-        System.out.println("Hello "+name);
+        // String name = new String("Sanish");
+        // name = name + " Kumar";
+        // System.out.println("Hello "+name);
+
+
+        StringBuffer sb = new StringBuffer("Sanish");
+        sb.append(" Kumar");
+
+        sb.insert(6, " Java ");
+        sb.setLength(30);
+
+        System.out.println(sb);
+
     }
 }
+
+// Mutable Strings
+// Immutable Strings
+// String Buffer(HERE)
+// String Builder
