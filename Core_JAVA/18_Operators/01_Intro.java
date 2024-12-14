31ad71fb65a3bbf33aaa1c673ae3ff519f808e3c
SanishKumar <18sanishkumar@gmail.com>
2024-12-14 08:42:11 +0530

    Bit Manipulation Revise

--- Core_JAVA/18_Operators/01_Intro.java
+++ Core_JAVA/18_Operators/01_Intro.java
@@ -1,5 +1,15 @@
+// Get Bit
+
 class Intro {
     public static void main(String[] args) {
-        
+        int n = 5;
+        int pos = 2;
+        int bitmask = 1 << pos;
+        System.out.println(n & bitmask);
     }
 }
+
+// H.W.
+// Set Bit
+// Clear Bit
+// Update Bit
